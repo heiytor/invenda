6425b0d868e612bb3c9e39eb4d3189ddeb6d5de7
Heitor Danilo <heitor.danilo@icloud.com>
2024-05-01 06:36:23 -0300

    flake support

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Invenda development flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    };
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 22;
+      overlays = [
+        (final: prev: rec {
+          go = prev."go_1_${toString goVersion}";
+          nodejs = prev.nodejs_latest;
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zsh
+            go_1_22
+            gotools
+            golangci-lint
+            gopls
+            node2nix
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Entering Invneda development environment."
+            echo "`go version`"
+            echo "node version `node --version`"
+            exec zsh
+          '';
+        };
+      });
+    };
+}
